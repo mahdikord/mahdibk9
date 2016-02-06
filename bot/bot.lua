db2181b190e6b90447ee5bc3436a07b648cdec1c
mahdikord <saeed.199429@gmail.com>
2016-02-06 14:05:27 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {165562299},
+    sudo_users = {180087690},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
